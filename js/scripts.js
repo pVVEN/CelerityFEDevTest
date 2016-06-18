c98df02a8e5e7fa2380fb36ab6d2b98bf0aea33c
Owen McNamara <owencmcnamara@gmail.com>
2016-06-18 15:14:35 -0400

    more edits to header

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*
+scripts.js
+by
+Owen McNamara
+*/
+/*
+var main = angular.module('main', ['ngRoute']);
+
+main.config(function($routeProvider)
+{
+	$routeProvider
+		.when('/', {
+			templateUrl : 'partials/main.html',
+			controller  : 'mainController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+*/
